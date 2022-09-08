2fee1a09fd0ae5c70074a2742edca5c60f1231ac
Joseph Pender <joey@ionic.io>
2022-09-08 11:14:39 -0500

    re-adding example app

--- apps/example-app/ios/App/Pods/Target Support Files/CoreCapacitor/CoreCapacitor-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CoreCapacitor : NSObject
-@end
-@implementation PodsDummy_CoreCapacitor
-@end
--- apps/example-app/node_modules/core-swift/README.md
+++
@@ -1,3 +0,0 @@
-# core-swift
-
-A description of this package.
